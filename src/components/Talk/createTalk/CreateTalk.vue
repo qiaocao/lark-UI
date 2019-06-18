7fea0c2d5fc4a8a79232f9acdb2cab612c4e038c
jihainan <alwaysbegoing@163.com>
2019-06-18 10:13:17 +0800

    更新群组创建

--- src/components/Talk/createTalk/CreateTalk.vue
+++ src/components/Talk/createTalk/CreateTalk.vue
@@ -1,13 +1,61 @@
 <template>
-    
+  <a-card :bordered="false">
+    <a-steps class="steps" :current="currentTab">
+      <a-step title="填写基本信息" />
+      <a-step title="选择群组成员" />
+      <a-step title="完成" />
+    </a-steps>
+    <div class="content">
+      <BaseInfo v-if="currentTab === 0" @nextStep="nextStep"/>
+      <UserSelect v-if="currentTab === 1" @nextStep="nextStep" @prevStep="prevStep"/>
+      <ResultPage v-if="currentTab === 2" @prevStep="prevStep" @finish="finish"/>
+    </div>
+  </a-card>
 </template>
 
 <script>
-export default {
+import BaseInfo from './BaseInfo'
+import UserSelect from './UserSelect'
+import ResultPage from './ResultPage'
 
+export default {
+  name: 'StepForm',
+  components: {
+    BaseInfo,
+    UserSelect,
+    ResultPage
+  },
+  data () {
+    return {
+      currentTab: 0,
+
+      // form
+      form: null
+    }
+  },
+  methods: {
+
+    // handler
+    nextStep () {
+      if (this.currentTab < 2) {
+        this.currentTab += 1
+      }
+    },
+    prevStep () {
+      if (this.currentTab > 0) {
+        this.currentTab -= 1
+      }
+    },
+    finish () {
+      this.currentTab = 0
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+  .steps {
+    max-width: 750px;
+    margin: 16px auto;
+  }
 </style>
