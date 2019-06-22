66930cf7f4729e545137c844b8d82632d5b82fe5
jihainan <alwaysbegoing@163.com>
2019-06-22 14:44:03 +0800

    修改群组展示部分的属性

--- src/store/modules/talk.js
+++ src/store/modules/talk.js
@@ -94,7 +94,7 @@ function setMessageInfo (id, talkMap, recentContact) {
  * @param {String} sender 发送者
  */
 function syncUnread2Server (online, reviser, sender) {
-  // 连接断开，添加提醒
+  // TODO: 连接断开，添加提醒
   if (!online) return
   const socketMessage = new SocketMessage({
     code: 9,
