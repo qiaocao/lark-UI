1aa77a693d8df4953abea0b11045ec7230ee7f5c
jihainan <alwaysbegoing@163.com>
2019-07-01 16:45:14 +0800

    修改用户选择组件，直接展示三级单位

--- src/components/Talk/createTalk/UserSelect.vue
+++ src/components/Talk/createTalk/UserSelect.vue
@@ -3,7 +3,7 @@
     <a-form style="max-width: 650px; margin: 40px auto 0;">
       <a-alert
         :closable="true"
-        message="只能选择与群组密级相符合的用户"
+        message="下列用户已根据群组密级进行了筛选"
         style="margin-bottom: 24px;"
       />
 
@@ -40,7 +40,7 @@ export default {
       loading: false,
       userList: [],
       listStyle: {
-        width: '200px', height: '450px'
+        width: '190px', height: '450px'
       }
     }
   },
