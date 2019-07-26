de2367bbb1e8a01840c0a7ff2b4add33c7769417
jihainan <alwaysbegoing@163.com>
2019-07-26 16:02:01 +0800

    迁移消息超时定时器的开启位置 > 解决页面刷新定时器消失的问题

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -86,7 +86,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions(['Login', 'StartMessageTimer']),
+    ...mapActions(['Login']),
     // handler
     handleUsernameOrEmail (rule, value, callback) {
       const { state } = this
@@ -127,8 +127,6 @@ export default {
     },
     loginSuccess (res) {
       this.$router.push({ path: '/dashboard/workplace' })
-      // 开启消息超时定时器
-      this.StartMessageTimer()
       this.$nextTick(() => {
         this.$notification.success({
           message: '欢迎',
